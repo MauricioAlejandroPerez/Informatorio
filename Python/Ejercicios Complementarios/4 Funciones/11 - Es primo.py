df4839b61537f85f140c52857db110cd2fa5a976
Mauricio Alejandro Pérez <map.j91@gmail.com>
2022-07-23 02:45:00 -0300

    22-7-22.2

--- Python/Ejercicios Complementarios/4 Funciones/11 - Es primo.py
+++ Python/Ejercicios Complementarios/4 Funciones/11 - Es primo.py
@@ -4,3 +4,10 @@ que determine si su parámetro es primo o no, devolviendo True si lo es y False 
 principal que lea un número entero del usuario y muestre un mensaje que indique si es primo o no. Asegúrese de que el 
 programa principal no se ejecutará si el archivo que contiene su solución se importa a otro programa.
 '''
+def es_primo(num):
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(es_primo(int(input())))
